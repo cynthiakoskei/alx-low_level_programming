7d7ab8d601c525f0372a464ab134633222fed442
cynthiakoskei <koskei.thia@gmail.com>
2023-07-04 10:07:57 -0700

    check alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - check alphabetic character
+ * return: always 0
+ */
+int _isalpha(int c);
+{
+	if ((c = _isalpha('h')) || (c = _isalpha('H')))
+	{
+		return (1);
+	}
+	else if
+	{
+		return (0);
+	}
+}
