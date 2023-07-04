00827671d1366a9bb05103dbc1aaa3b6f4e210d4
cynthiakoskei <koskei.thia@gmail.com>
2023-07-04 09:25:13 -0700

    alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,13 @@
 
 /**
  * main - starting point
+ * program to print alphabet
  * return: 0 if successful
  */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
